eaabef664a316bbaea0b2b50dd83cdd20ea1d00e
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-03 16:21:13 +0600

    rendering data added from API

--- index.js
+++ index.js
@@ -48,32 +48,47 @@ const displayCategoryBtn = (data) => {
 const displayVideos = (data) => {
     const videosBox = document.getElementById('videosDiv');
     videosBox.innerHTML = '';
-    
-    data.map((item) =>{
+
+    data.map((item) => {
         // console.log(item);
-        console.log(item.thumbnail);
+        // console.log(item.title);
+        console.log(item.others.posted_date);
+        // console.log(item.authors[0].verified);
+        // <img src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" class="w-4 h-4" alt=""></img>
+
         videosBox.innerHTML +=
-        `
+            `
             <div class="card card-compact bg-base-100 w-full">
-                <figure class="h-[200px]">
+                <figure class="h-[200px] relative">
                     <img src="${item.thumbnail}" class="w-full h-full object-cover" alt="Shoes" />
+                    <div class="absolute right-1 bottom-2 rounded bg-[#171717] text-[12px] font-normal text-white ">
+                    <div>${item.others.posted_date !== '' ?  getTime(item.others.posted_date) : ''}</div>
+                    </div>
                 </figure>
                 <div class="py-2 flex gap-2">
-                    <div class="w-[10%] rounded-full"><img src="asset/Logo.png" class="w-full" alt=""></div>
+                    <div class="w-[9%] rounded-full h-7"><img src="${item.authors[0].profile_picture}" class="w-full rounded-[100%] h-full object-cover"  alt=""></div>
                     <div class="space-y-2">
-                        <h1 class="font-bold text-base text-[#171717]">Building a Winning UX Strategy Using the Kano Model</h1>
+                        <h1 class="font-bold text-base text-[#171717]">${item.title}</h1>
                         <div class="font-normal text-sm text-[#171717]/[0.7] flex items-center gap-2">
-                            <h1>Awlad Hossain</h1><i><img src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" class="w-4 h-4" alt=""></i>
+                            <h1>${item.authors[0].profile_name}</h1>
+                            <i>${item.authors[0].verified == true ? `<img src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" class="w-4 h-4" alt=""></img>` : ``}</i>
                         </div>
-                        <p  class="font-normal text-sm text-[#171717]/[0.7]">91K views</p>
+                        <p  class="font-normal text-sm text-[#171717]/[0.7]">${item.others.views}</p>
                     </div>
 
                 </div>
             </div>
     `
-        
+
     })
-    
+
+}
+//  time conversion
+const getTime = (string) => {
+    const hours = parseInt(string / 3600);
+    const min = parseInt((string % 3600) / 60);
+    return (` ${hours} hrs ${min} min ago `);
+
 }
 
 categoryFetching();
