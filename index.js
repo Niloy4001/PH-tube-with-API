de53d8167b52b49c96d2937631f88420dfee9aad
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-03 12:10:27 +0600

    category section added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+const categoryFetching = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories');
+    const data = await res.json();
+    displayCategoryBtn(data.categories)
+    
+}
+
+const displayCategoryBtn = (data) =>{
+    const categoryBtnDiv = document.getElementById('category-btn');
+    data.map((item)=>{
+        categoryBtnDiv.innerHTML += `
+            <button class="btn"> ${item.category} </button>
+        `
+        
+    })
+    
+}
+
+categoryFetching();
